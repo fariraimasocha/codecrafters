48139c898f9e9d290bf44229b7a078b4fd0ae882
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2023-05-15 12:15:51 +0200

    footer image
    customize footer

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -159,8 +159,24 @@
 
     </div>
 
-    <footer class="w-full h-32 bg-white">
-
-    </footer>
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between h-2/6">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
+                class="hover:underline">CODECRAFTERS™</a>. All Rights Reserved.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Crafts</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Projects</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">Contact Me On:</a>
+            </li>
+        </ul>
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">fariraimasocha@gmail.com</span><span
+            class="text-sm text-white sm:text-center">+263781840930</span>
+        <img src="{{ asset('images/codecrafters logo.png') }}" alt="logo" width="38%" height="38%">
+    </div>
 
 </body>
