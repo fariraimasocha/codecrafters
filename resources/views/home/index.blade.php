ef55e66a3d2524031f41a2fe7d16387fd9030c43
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2023-05-15 13:30:58 +0200

    footer
    custom footer

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -160,6 +160,7 @@
     </div>
 
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between h-2/6">
+        <img src="{{ asset('images/codecrafters logo.png') }}" alt="logo" width="25%" height="25%">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
                 class="hover:underline">CODECRAFTERS™</a>. All Rights Reserved.
         </span>
@@ -176,7 +177,6 @@
         </ul>
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">fariraimasocha@gmail.com</span><span
             class="text-sm text-white sm:text-center">+263781840930</span>
-        <img src="{{ asset('images/codecrafters logo.png') }}" alt="logo" width="38%" height="38%">
     </div>
 
 </body>
